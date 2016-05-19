91ca45995869e64cb0a2a68cfc7a4f39c7e59106
LuoZhangKa <LuoZhangKa@qlcoder.com>
2016-05-19 23:34:30 +0800

    add function

--- source.py
+++ source.py
@@ -18626,3 +18626,5 @@ def f_31db0d(a,b,c,d,e,f,g,h,i,j):
     return (a-b+c*d*e*f*g+h+i-j+f_245640c(a,b,c)+f_1635c1c(a,b,c,d,e,f,g,h)+f_11411e1(a,b,c,d))%10
 def f_2be05c2(a,b,c,d,e,f,g,h,i):
     return (a+b*c+d+e*f+g*h-i+f_2dcb1d(a,b,c)+f_16a9f8f(a,b,c,d,e,f,g,h)+f_588849(a,b))%10
+def f_34b24a7(a,b,c,d,e,f,g,h):
+    return (a+b-c+d+e-f+g-h+f_4dfa235(a,b,c,d,e,f,g,h,307)+f_f87234(a,b,c,d,e,f,g,h)+f_548a8ec(a,b,c,d,e,f,g,h,235))%10
--- unit.py
+++ unit.py
@@ -6,6 +6,6 @@ import source
 
 class mytest(unittest.TestCase):
     def test(self):
-        self.assertEqual(source.f_2be05c2(1,2,3,4,5,6,7,8,9),1,'fail')
+        self.assertEqual(source.f_34b24a7(1,2,3,4,5,6,7,8),4,'fail')
 if __name__ == '__main__':
     unittest.main()
