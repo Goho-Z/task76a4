c0072983dd7a79228c9d70be97bdd424b9a21d7e
ChenAWei <ChenAWei@qlcoder.com>
2016-05-19 23:22:34 +0800

    add function

--- source.py
+++ source.py
@@ -7898,3 +7898,5 @@ def f_5a0bb50(a,b,c,d,e,f):
     return (a+b*c+d*e+f+f_574b9d(a,b,c,d,e)+f_1e646f1(a,b,c,d)+f_420f437(a,b))%10
 def f_15fd0b2(a,b,c,d,e,f,g,h):
     return (a-b*c-d*e+f*g*h+f_576246f(a,b,c,d,e,f,g,h))%10
+def f_29fe00b(a,b,c,d,e,f):
+    return (a*b*c+d*e+f+f_2f4744f(a,b,c))%10
