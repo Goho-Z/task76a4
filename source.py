1daf0eccc29792ed49a69598165abf099e5ff9de
NongChengLi <NongChengLi@qlcoder.com>
2016-05-19 23:19:11 +0800

    add function

--- source.py
+++ source.py
@@ -2222,3 +2222,5 @@ def f_1895d99(a):
     return (a+f_747cac(a,76,279,490,182,48,601,335))%10
 def f_1f0f0d1(a,b,c,d,e,f,g,h):
     return (a*b+c-d+e*f*g-h+f_3de78b0(a,b,c,d,e,f,g,h,924,76))%10
+def f_4f650f(a,b,c,d,e,f,g,h,i,j):
+    return (a+b*c*d+e-f+g-h*i*j+f_3b1468(a,b,c,d,e)+f_4dcb8bb(a,b,c,d,e,f,g,h,i,j)+f_4d12f2a(a,b))%10
