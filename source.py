4ed80fc65365c9de44d7c059e8460954b6553a1b
LengXue <LengXue@qlcoder.com>
2016-05-19 23:18:55 +0800

    add function

--- source.py
+++ source.py
@@ -1636,3 +1636,5 @@ def f_3656ab8(a,b,c,d):
     return (a-b-c+d+f_47c4d0c(a))%10
 def f_4948d4b(a,b,c,d,e):
     return (a+b-c-d*e+f_1b06142(a,b,c,d)+f_5318064(a,b,c,d,e,15,462,296,925))%10
+def f_52be31d(a,b,c,d,e):
+    return (a+b*c-d-e+f_29272b(a,b,c,d)+f_4b80977(a,b,c,d,e,563))%10
