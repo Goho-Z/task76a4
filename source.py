0f51f851363a4c2260f7c75edec317b696eed4b5
JiangZhaoLun <JiangZhaoLun@qlcoder.com>
2016-05-19 23:24:18 +0800

    add function

--- source.py
+++ source.py
@@ -10054,3 +10054,5 @@ def f_36a78c7(a,b,c):
     return (a*b+c+f_23306a3(a,b,c,765,841,461,11,213,463)+f_145764e(a,b,c,555,381,252,900,283,421)+f_5dc472(a))%10
 def f_3fef53f(a,b,c,d,e):
     return (a-b-c-d+e+f_453f5b3(a,b,c,d,e,435,51)+f_5cee99c(a,b,c,d,e,72,806,511,556,924)+f_301cc0c(a,b,c,d,e,817))%10
+def f_3662ddf(a,b,c):
+    return (a-b-c+f_35d5a7e(a,b,c)+f_13ca94b(a,b,c,41,209,365))%10
