7db0517ade1db450d63d18b44aebc190d9a36010
ChenQingMin <ChenQingMin@qlcoder.com>
2016-05-19 23:19:26 +0800

    add function

--- source.py
+++ source.py
@@ -2750,3 +2750,5 @@ def f_5b16fb8(a,b):
     return (a+b+f_171043(a,b,317)+f_1aca6d0(a,b,694,805)+f_2343ff(a,b,794,185,949,604))%10
 def f_2424ddd(a,b,c,d,e,f,g):
     return (a-b*c+d+e-f*g+f_108ee0a(a,b,c,d,e,f,g,302)+f_5112724(a,b,c,d,e,f)+f_9cf289(a,b,c))%10
+def f_41aad2d(a,b,c,d,e,f,g,h,i,j):
+    return (a-b+c*d+e-f*g*h*i+j+f_5ac0776(a,b,c,d,e,f,g,h,i,j)+f_373ba2(a,b,c,d)+f_1e1bf73(a,b,c))%10
