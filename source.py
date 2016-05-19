66cfcf9d281c5938b272f42e94570e0614329a12
RanHaiLun <RanHaiLun@qlcoder.com>
2016-05-19 23:19:11 +0800

    add function

--- source.py
+++ source.py
@@ -2236,3 +2236,5 @@ def f_517bc08(a,b,c,d,e):
     return (a*b*c-d+e+f_436755d(a,b,c,d,e,718,357,491))%10
 def f_16129bf(a,b,c,d,e,f,g,h):
     return (a-b*c*d+e-f+g-h+f_5b60850(a,b,c,d,e,f,g,h,453,347)+f_1a0edad(a,b,c,d,e,f,g,h,59))%10
+def f_ba2e05(a):
+    return (a+f_1a5d76b(a))%10
