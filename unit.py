aaf67a841701018bf3c526d4c2a19d26ac5deee3
LiKuaiMei <LiKuaiMei@qlcoder.com>
2016-05-19 23:27:18 +0800

    add function

--- unit.py
+++ unit.py
@@ -6,6 +6,6 @@ import source
 
 class mytest(unittest.TestCase):
     def test(self):
-        self.assertEqual(source.f_4e9c79(1,2,3,4),10,'fail')
+        self.assertEqual(source.f_3800df7(1,2,3,4,5,6,7,8,9,10),2,'fail')
 if __name__ == '__main__':
     unittest.main()
