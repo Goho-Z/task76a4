baaccd1e89658160cf0e9443034601b6dddd6968
LanXuPing <LanXuPing@qlcoder.com>
2016-05-19 23:24:54 +0800

    add function

--- source.py
+++ source.py
@@ -10734,3 +10734,5 @@ def f_350a3aa(a,b,c,d,e,f,g):
     return (a+b*c-d*e+f+g+f_2419ab0(a,b,c,d,e,f)+f_4d1301d(a,b,c)+f_3ceb701(a,b,c,d))%10
 def f_b8df98(a,b):
     return (a*b+f_10d4252(a,b,59,928,605,556,965,705,497)+f_16dc44f(a,b,195,181,54,164,972,43,85)+f_345a8a2(a,b,56,261,358,972,264,684,201,122))%10
+def f_4f6242b(a,b,c,d,e,f,g,h,i,j):
+    return (a+b*c*d+e*f*g*h*i*j+f_18462ef(a,b,c))%10
