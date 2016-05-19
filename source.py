8b52d8c19aaa81f73986eb69bcb509c740dc81f8
YuMingFang <YuMingFang@qlcoder.com>
2016-05-19 23:20:38 +0800

    add function

--- source.py
+++ source.py
@@ -4950,3 +4950,5 @@ def f_288e5e6(a,b,c,d,e):
     return (a+b-c-d-e+f_5e4aad1(a,b)+f_d7379d(a))%10
 def f_2f3284e(a,b,c,d,e,f,g,h):
     return (a+b-c-d*e-f*g*h+f_58cff5e(a,b,c,d,e)+f_246cc61(a,b,c,d,e,f,g,h,104)+f_548d0b8(a,b,c,d,e,f,g))%10
+def f_170c599(a):
+    return (a+f_2ab7122(a,746,694,90,386))%10
