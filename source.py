71d9120f9d0d0aacb7c3939ebf81c5965b302674
MoYouRong <MoYouRong@qlcoder.com>
2016-05-19 23:21:25 +0800

    add function

--- source.py
+++ source.py
@@ -6212,3 +6212,5 @@ def f_3249772(a,b,c,d):
     return (a*b*c*d+f_114fbd0(a,b,c)+f_2ebb214(a,b,c,d,986,945,985,571,503,840))%10
 def f_691610(a,b,c,d,e,f,g,h,i):
     return (a-b*c-d+e-f+g+h-i+f_921cfd(a,b,c,d,e,f,g,h,i,679)+f_50c465f(a,b,c,d,e,f,g,h,i)+f_17184fc(a))%10
+def f_33f0bc2(a,b,c,d,e,f,g):
+    return (a+b-c-d-e+f*g+f_fd9310(a,b,c,d,e,f,g,70)+f_5ba2cbc(a)+f_e3c21d(a))%10
