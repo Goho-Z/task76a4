10e1331679ee23089a84015d42f0a4c2fe179c4d
KongLongSheng <KongLongSheng@qlcoder.com>
2016-05-19 23:24:25 +0800

    add function

--- source.py
+++ source.py
@@ -10188,3 +10188,5 @@ def f_2cfa264(a,b,c,d,e,f):
     return (a+b-c+d*e-f+f_4582f9e(a,b,c,d,e,f)+f_3311348(a,b)+f_43f1bf2(a))%10
 def f_1fe23f7(a,b):
     return (a+b+f_b41019(a,b)+f_3f67c45(a,b,689,847,388,76,998,663)+f_2fc6ec6(a))%10
+def f_b00579(a,b,c,d,e,f,g,h):
+    return (a*b+c-d-e*f+g-h+f_362255e(a,b,c,d,e,f,g,h,755)+f_2eb7a08(a,b,c,d,e,f,g,h)+f_bea1ca(a,b,c))%10
