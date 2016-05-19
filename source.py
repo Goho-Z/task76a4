0677038adb23104e21edf90149f80e3f3b86d065
WangRuiBing <WangRuiBing@qlcoder.com>
2016-05-19 23:20:47 +0800

    add function

--- source.py
+++ source.py
@@ -5218,3 +5218,5 @@ def f_70fb0c(a,b,c,d):
     return (a-b*c*d+f_fa5c28(a,b,c)+f_58ceeb(a,b,c,d))%10
 def f_42dcf6c(a,b,c):
     return (a*b-c+f_1441969(a,b)+f_24b75b(a,b,c,755,905)+f_434092a(a,b,c,531,843,595,761,325,895,191))%10
+def f_132c5c(a,b):
+    return (a+b+f_3ae4a8c(a,b,534)+f_b2c9db(a))%10
