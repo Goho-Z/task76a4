28334dca0e93ccea44086afb03708f12c878fc3a
HuangBangLian <HuangBangLian@qlcoder.com>
2016-05-19 23:32:16 +0800

    add function

--- source.py
+++ source.py
@@ -17034,3 +17034,5 @@ def f_445ba5e(a,b,c,d,e,f,g,h,i,j):
     return (a-b+c+d+e+f-g-h-i+j+f_362255e(a,b,c,d,e,f,g,h,i)+f_26d28f4(a,b,c,d,e,f)+f_546451c(a,b,c,d,e))%10
 def f_24b6ba5(a,b,c,d,e,f,g,h,i):
     return (a*b*c*d+e*f*g*h*i+f_c5e0b3(a,b,c)+f_4757edd(a,b,c,d,e,f)+f_f89663(a,b))%10
+def f_5f1be9b(a,b,c,d,e,f,g,h,i,j):
+    return (a*b*c-d-e-f-g+h+i*j+f_49a8c5f(a,b,c,d,e,f,g,h)+f_6fff61(a,b,c,d,e,f))%10
