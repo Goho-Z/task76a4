9505b7458eced563a526c9f8d305eadafac9e288
LeiWeiZe <LeiWeiZe@qlcoder.com>
2016-05-19 23:18:33 +0800

    add function

--- source.py
+++ source.py
@@ -658,3 +658,5 @@ def f_583b5a5(a,b,c,d,e):
     return (a*b+c*d*e+f_2fb588f(a,b)+f_80b922(a,b,c,d,e,704)+f_4d12f2a(a,b))%10
 def f_eda041(a):
     return (a+f_5accc7d(a,3)+f_3915a58(a,513,467,147,981,60,599,400,332)+f_2e3793d(a,800,116,608,381,283,901,591,353,266))%10
+def f_d7876e(a,b,c,d,e,f,g,h,i):
+    return (a-b*c-d+e*f+g+h-i+f_5e6c9d6(a,b,c,d,e,f,g,h))%10
