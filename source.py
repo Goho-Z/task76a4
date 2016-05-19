b20338bbf36001de51ef2c1a27d721a442de3e30
ZhaoShouCai <ZhaoShouCai@qlcoder.com>
2016-05-19 23:18:29 +0800

    add function

--- source.py
+++ source.py
@@ -474,3 +474,5 @@ def f_48e7b28(a,b,c):
     return (a-b*c+f_25fb1da(a,b,c,857,856,527,718)+f_fa5c28(a,b,c)+f_271d934(a,b,c,335,31,655,627))%10
 def f_431d328(a,b):
     return (a-b+f_cef9d5(a,b,886,638,92,596))%10
+def f_8caae1(a,b,c,d,e):
+    return (a+b-c-d+e+f_1026acb(a,b))%10
