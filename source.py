eeacc38315c2e40933db50d39a20d0addd3d9b5b
SongYuSheng <SongYuSheng@qlcoder.com>
2016-05-19 23:25:35 +0800

    add function

--- source.py
+++ source.py
@@ -11454,3 +11454,5 @@ def f_501a02c(a,b,c,d,e):
     return (a+b+c-d-e+f_1d98e90(a))%10
 def f_31f62ef(a,b,c,d):
     return (a-b-c*d+f_3821b55(a,b,c,d,431)+f_310a1dd(a,b,c,d,242,821,399)+f_4458bac(a,b,c,d,248,712,876))%10
+def f_3b62b83(a,b,c,d,e,f,g,h):
+    return (a*b*c*d*e-f*g*h+f_52873ff(a,b,c,d,e))%10
