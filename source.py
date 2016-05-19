9a87b66e5d6aad49fa9cde5b74bcaf29288c511a
YangHaiBing <YangHaiBing@qlcoder.com>
2016-05-19 23:18:55 +0800

    add function

--- source.py
+++ source.py
@@ -1604,3 +1604,5 @@ def f_4ad426b(a,b,c,d,e,f,g,h,i):
     return (a+b+c*d*e-f-g-h-i+f_5dcb737(a,b,c,d)+f_35cf31d(a,b,c,d,e))%10
 def f_290348b(a,b,c,d):
     return (a-b+c-d+f_20d3ec1(a,b,c,d,564,811,120,673)+f_2807d02(a,b,c,d,559,923,544,587,105,895)+f_31ee089(a,b,c,d))%10
+def f_2c9a059(a,b):
+    return (a-b+f_11537ea(a,b,160,147,89,701)+f_554e519(a,b,439,983,289,181,670)+f_1a364a8(a,b))%10
