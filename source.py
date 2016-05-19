404ab21db3920a38e6ded13eb2b6d1aca0807f54
WangKunShan <WangKunShan@qlcoder.com>
2016-05-19 23:22:33 +0800

    add function

--- source.py
+++ source.py
@@ -7860,3 +7860,5 @@ def f_546451c(a,b,c,d,e):
     return (a+b+c+d*e+f_73e136(a,b,c))%10
 def f_3db3334(a,b,c,d,e,f,g,h,i,j):
     return (a+b+c+d-e+f-g*h-i+j+f_35cf31d(a,b,c,d,e)+f_2804243(a,b,c,d,e,f,g)+f_55770ad(a,b,c))%10
+def f_25c0c3(a,b,c,d):
+    return (a+b+c-d+f_3dfa1a9(a,b,c,d,374)+f_393826f(a,b)+f_3e22e06(a,b,c,d,715,748,555))%10
