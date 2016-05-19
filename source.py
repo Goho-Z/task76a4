88dccd64d23d66300ad59a660bacb678904f5e66
PengChangXian <PengChangXian@qlcoder.com>
2016-05-19 23:18:20 +0800

    add function

---
+++ source.py
@@ -0,0 +1,2 @@
+def f_3fc2849(a,b,c,d,e,f):
+    return (a*b-c*d*e-f)%10
---
+++ unit.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+# encoding: utf-8
+import unittest
+import source
+
+
+class mytest(unittest.TestCase):
+    def test(self):
+        self.assertEqual(source.f_3fc2849(1,2,3,4,5,6),6,'fail')
+if __name__ == '__main__':
+    unittest.main()
