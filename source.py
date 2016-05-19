3afc8e551a64de82c56c30ebc10aeab161635da9
TaoWenCan <TaoWenCan@qlcoder.com>
2016-05-19 23:21:55 +0800

    add function

--- source.py
+++ source.py
@@ -6988,3 +6988,5 @@ def f_37c251(a,b,c,d,e,f,g,h,i,j):
     return (a+b*c*d+e*f-g*h*i+j+f_404c747(a,b,c,d)+f_1e9b614(a,b,c,d,e,f,g)+f_28bbdb6(a,b,c))%10
 def f_197d80b(a,b,c):
     return (a*b+c+f_5b8f22b(a,b,c,229,931,190,202,356,81,60)+f_91091f(a,b,c,195,679,48,655))%10
+def f_53bb9ff(a,b,c,d,e,f,g,h,i):
+    return (a+b+c+d+e+f-g*h+i+f_554e519(a,b,c,d,e,f,g))%10
