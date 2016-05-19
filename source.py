641445c4469a4f78c0e204b44cce77fb8e7b116e
MaoYiLiang <MaoYiLiang@qlcoder.com>
2016-05-19 23:22:46 +0800

    add function

--- source.py
+++ source.py
@@ -8154,3 +8154,5 @@ def f_244dc9e(a,b,c,d,e,f,g,h,i):
     return (a*b*c-d+e*f-g*h+i+f_1d70611(a,b,c,d,e,f))%10
 def f_5150361(a,b,c,d):
     return (a-b*c-d+f_d94493(a,b,c)+f_1c2dcde(a,b,c,d))%10
+def f_4707f11(a,b,c,d,e,f,g):
+    return (a+b*c*d-e+f+g+f_541caf9(a,b,c,d,e,f,g,307,688,750))%10
