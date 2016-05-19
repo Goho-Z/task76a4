fdcae33de3a1972787f8bd37fe253ecc70cb1ffc
ChenZhenTong <ChenZhenTong@qlcoder.com>
2016-05-19 23:29:36 +0800

    add function

--- source.py
+++ source.py
@@ -15092,3 +15092,5 @@ def f_139a61b(a,b,c,d,e,f,g,h):
     return (a-b-c+d*e+f-g-h+f_2c7aa28(a,b,c,d,e,f,g,h,515,203))%10
 def f_32520d7(a):
     return (a+f_6dd0f7(a,972,373,678,933,533)+f_3afd724(a,806,580,430,527,940,187,447)+f_be642e(a))%10
+def f_2b017fa(a,b,c,d,e,f):
+    return (a-b*c-d+e+f+f_3b1468(a,b,c,d,e)+f_3ee69a(a))%10
--- unit.py
+++ unit.py
@@ -6,6 +6,6 @@ import source
 
 class mytest(unittest.TestCase):
     def test(self):
-        self.assertEqual(source.f_32520d7(1),9,'fail')
+        self.assertEqual(source.f_2b017fa(1,2,3,4,5,6),10,'fail')
 if __name__ == '__main__':
     unittest.main()
