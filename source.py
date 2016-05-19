1d58ed204a8a765af37dd70c61c5cbb725ea405e
LiJieXiu <LiJieXiu@qlcoder.com>
2016-05-19 23:19:27 +0800

    add function

--- source.py
+++ source.py
@@ -2794,3 +2794,5 @@ def f_14f8a5d(a,b,c,d,e,f):
     return (a-b+c-d+e+f+f_436755d(a,b,c,d,e,f,457,193)+f_2d11cb5(a,b,c,d,e))%10
 def f_260056b(a,b,c,d):
     return (a-b-c*d+f_1fd3f02(a,b,c,d,139,164,138,197,136))%10
+def f_5b472d0(a,b,c,d,e,f,g,h,i,j):
+    return (a-b+c*d-e-f-g+h+i-j+f_58ac234(a,b,c,d,e,f,g,h,i))%10
